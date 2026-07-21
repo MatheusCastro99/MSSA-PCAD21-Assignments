af1b2a259cf805bde72ebcc60e5e801acb69b236
Matheus Castro <mpcastro0525@gmail.com>
2026-07-21 18:51:20 -0400

    - Week 3 Challenge Labs Finished

---
+++ Week3/W3ChallengeLabs/W3ChallengeLab3/Program.cs
@@ -0,0 +1,54 @@
+﻿// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+// Create nested loop that will iterate through each index at each index position
+// Because X + Y and Y + X are the same, each inner loop index only need to iterate through those in front of outer loop index
+//    - Set inner loop index to start at outer loop's index
+
+int[] nums;
+int targetNum;
+
+Console.Write("How many numbers do you want to input? ");
+nums = new int[Convert.ToInt32(Console.ReadLine())];
+BuildArray();
+
+Console.Write("What sum do you want to find? ");
+targetNum = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+
+FindSum(nums, targetNum);
+
+void FindSum (int[] nums, int targetNum)
+{
+    for (int i = 0; i < nums.Length; i++)
+    {
+        for (int j = i; j < nums.Length; j++)  //j = i because we do not need to check backwards
+        {
+            if (j == i)                        //Excercise constraint: you may not use the same element twice.
+            {                                  //   - Because j and i are iterating in the same array, j == i means nums[j] == nums[i]
+                continue;
+            }
+
+            if ((nums[i] + nums[j]) == targetNum)
+            {
+                Console.WriteLine("Those two indexes value add to the Target Number: ");
+                Console.WriteLine($"[{i}, {j}]");
+                Console.WriteLine($"The respective values are: {nums[i]}, and {nums[j]}");
+
+                return;
+            }
+        }
+    }
+
+    Console.WriteLine($"No addition combination in this list on number returns your target Number. -1");
+}
+
+void BuildArray()
+{
+    for (int i = 0; i < nums.Length; i++)
+    {
+        Console.Write("Enter number: ");
+        nums[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    Console.WriteLine();
+}
---
+++ Week3/W3ChallengeLabs/W3ChallengeLab4/Program.cs
@@ -0,0 +1,44 @@
+﻿//You are given a string s consisting only of uppercase English letters.
+//You can apply some operations to this string where, in one operation, you can remove any occurrence of one of the substrings "AB" or "CD" from s.
+
+//Return the minimum possible length of the resulting string that you can obtain.
+//Note that the string concatenates after removing the substring and could produce new "AB" or "CD" substrings.
+
+//EX: string s = "ABFCACDB"
+//step 1: remove AB from s -> "FCACDB". Step 2: remove CD from s -> "FCAB". Step 3: remove AB from s -> "FC"
+//Output s.Length -> 2
+
+//LINE OF THOUGHT =======================================================================================================================================================
+
+//Recursive method to check for occurrences of AB || CD in string s
+//   - if occurence is found, remove it from s and recall method with new s as argument
+//   - if occurence is not found, return s.Length
+//   - Opted for creating a bool IsOcurrenceFound so recursion will happen after a full iteration of the sentence, and not at each occurrence
+//Might want to keep original string and operational string separated
+
+string letters = "ABFCACDB";
+string lettersOps = letters;
+
+Console.WriteLine($"Initial string: {letters}\nFinal string: {lettersOps}\nMinimum Length {MinLength(lettersOps)}");
+
+int MinLength(string sentence)
+{
+    bool IsOcurrenceFound = false;
+
+    for (int i = 0; i < (lettersOps.Length-1); i++) // Length-1 to prevent out of bounds since I am checking simultaneously i and i+1
+    {
+        if ((lettersOps[i] == 'A' && lettersOps[i + 1] == 'B') || (lettersOps[i] == 'C' && (lettersOps[i + 1] == 'D')))
+        {
+            lettersOps = lettersOps.Remove(i, 2);
+            IsOcurrenceFound = true;
+          //Console.WriteLine("Occurrence found");
+        }
+    }
+
+    if (IsOcurrenceFound) //Recursion occurs after the whole string is iterated instead after each occurence is found
+    {
+        MinLength(lettersOps);
+    }
+
+    return lettersOps.Length;
+}
