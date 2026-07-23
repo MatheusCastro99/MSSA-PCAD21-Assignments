4bf9aab29d25381170e27b49f919b21ba2fb3a62
Matheus Castro <mpcastro0525@gmail.com>
2026-07-23 15:50:31 -0400

    - Improved Challenge Lab 4 to use Stack instead of recursion.

--- Week3/W3ChallengeLabs/W3ChallengeLab4/Program.cs
+++ Week3/W3ChallengeLabs/W3ChallengeLab4/Program.cs
@@ -17,29 +17,63 @@
 //   - Opted for creating a bool IsOcurrenceFound so recursion will happen after a full iteration of the sentence, and not at each occurrence
 //Might want to keep original string and operational string separated
 
-string letters = "ABFCACDB";
+//Stack method:
+//   - Pushes first char in the sequence to the stack
+//   - enters the loop that will compare the sequence[i] to the last char in the Stack with Stack.Peak()
+
+using System.Collections;
+
+string letters = "CCCCDDDD";
 string lettersOps = letters;
 
-Console.WriteLine($"Initial string: {letters}\nMinimum Length {MinLength(lettersOps)}\nFinal string: {lettersOps}");
+//Console.WriteLine($"Initial string: {letters}\nMinimum Length {MinLength(lettersOps)}\nFinal string: {lettersOps}");
+Console.WriteLine($"Initial string: {letters}\nMinimum Length {MinLengthWithStack(lettersOps)}\nFinal string: {lettersOps}");
 
-int MinLength(string sentence)
+
+//int MinLength(string sentence)
+//{
+//    bool IsOcurrenceFound = false;
+
+//    for (int i = 0; i < (lettersOps.Length-1); i++) // Length-1 to prevent out of bounds since I am checking simultaneously i and i+1
+//    {
+//        if ((lettersOps[i] == 'A' && lettersOps[i + 1] == 'B') || (lettersOps[i] == 'C' && (lettersOps[i + 1] == 'D')))
+//        {
+//            lettersOps = lettersOps.Remove(i, 2);
+//            IsOcurrenceFound = true;
+//          //Console.WriteLine("Occurrence found");
+//        }
+//    }
+
+//    if (IsOcurrenceFound) //Recursion occurs after the whole string is iterated instead after each occurence is found
+//    {
+//        return MinLength(lettersOps);
+//    }
+
+//    return lettersOps.Length;
+//}
+
+int MinLengthWithStack(string sentence)
 {
-    bool IsOcurrenceFound = false;
+    Stack<char> lettersStack = new("");
+    lettersStack.Push(sentence[0]);
 
-    for (int i = 0; i < (lettersOps.Length-1); i++) // Length-1 to prevent out of bounds since I am checking simultaneously i and i+1
+    for ( int i = 1; i < sentence.Length; i++ )
     {
-        if ((lettersOps[i] == 'A' && lettersOps[i + 1] == 'B') || (lettersOps[i] == 'C' && (lettersOps[i + 1] == 'D')))
+        if ((sentence[i] == 'B' && lettersStack.Peek() == 'A') || (sentence[i] == 'D' && lettersStack.Peek() == 'C'))
         {
-            lettersOps = lettersOps.Remove(i, 2);
-            IsOcurrenceFound = true;
-          //Console.WriteLine("Occurrence found");
+            lettersStack.Pop();
+
+            if(lettersStack.Count == 0 && (i+1<sentence.Length)) //If stack becomes empty after pop() and next index is not out of bounds
+            {                                                    // pushes the next index to the Stack (Push(sentence[i+1])) and skips the iteration for that char (i++)
+                lettersStack.Push(sentence[i + 1]);
+                i++;
+            }
+            continue;
         }
+
+        lettersStack.Push(sentence[i]);
     }
 
-    if (IsOcurrenceFound) //Recursion occurs after the whole string is iterated instead after each occurence is found
-    {
-        return MinLength(lettersOps);
-    }
-
-    return lettersOps.Length;
+    lettersOps = string.Join("", lettersStack);
+    return lettersStack.Count;
 }
