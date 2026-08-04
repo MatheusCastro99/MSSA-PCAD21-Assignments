2656837609a918aa6849712e4454a021445508f7
Matheus Castro <mpcastro0525@gmail.com>
2026-08-04 17:57:51 -0400

    - Assignments 5.2.x added

---
+++ Week 5 - Intro Data Structure and Algorithm/Assignments_5.2/Assignment5.2.x/Program.cs
@@ -0,0 +1,89 @@
+﻿#region ASSIGNMENT 5.3.1
+//Given a string s consisting of words and spaces, return the length of the last word in the string.
+//A word is a maximal substring consisting of non-space characters only.
+
+string s = "fly me to the moon   ";
+int lastWordLength = 0;
+
+bool startCount = false; //Will help accounting for leading spaces
+
+Stack<char> lastWord = new();
+
+for (int i = s.Length-1;  i >= 0; i--) //Initializes the loop at the last index position of s and iterate backwards
+{
+    if (s[i] == ' ' && !startCount) continue; //Accounts for leading spaces, iterating backwards without incrementing lastWordLength;
+
+    if (s[i] == ' ' && startCount) break; //Accounts for beginning of last word, marked by a ' '. Breaks out of loop without incrementing
+
+    startCount = true; //This line will only be reached when iteration fails two lasts checks, marking the last character of the last word
+    lastWordLength++; //From here, startCount will be true so it will fail the first test every time.
+
+    lastWord.Push(s[i]); //Stacks are LIFO, so even though the characters are being stored in reverse order, they will result in the actual word when deconstructed
+}
+
+foreach (char c in lastWord) Console.Write(c); //Prints lastWord preserving the Stack
+
+Console.Write($" Contains {lastWordLength} letters");
+#endregion
+
+Console.WriteLine("\n==================================================\n");
+
+#region ASSIGNMENT 5.3.2 and 5.3.3
+//5.3.2: Write a program in C# Sharp to print the first n natural number using recursion.
+//5.3.3: Write a program in C# Sharp to print numbers from n to 1 using recursion.
+
+int n = 10;
+PrintNaturalNumbersIncrement(n);
+Console.WriteLine("\n");
+PrintNaturalNumbersDecrement(n);
+
+void PrintNaturalNumbersIncrement(int n)
+{
+    if (n > 0)
+    {
+        PrintNaturalNumbersIncrement(n - 1); //Recursion comes first, so first Console.Write reached is from the last recursion, n==1
+
+        Console.Write($"{n} ");
+    }
+}
+
+void PrintNaturalNumbersDecrement(int n)
+{
+    if (n > 0)
+    {
+        Console.Write($"{n} "); //Printing comes first, so now the numbers will print before decreasing
+        PrintNaturalNumbersDecrement(n - 1);
+    }
+}
+#endregion
+
+Console.WriteLine("\n\n==================================================\n");
+
+#region ASSIGNMENT 5.3.4
+//Write a program in C# Sharp to check whether a given string is Palindrome or not using recursion.
+//Method will require two arguments, left and right, if at any point they do not match, return false
+//On match, call the method moving the poiters towards each other.
+
+Console.WriteLine("Enter to check if it is a palindrome recursivelly");
+string inputString = Console.ReadLine();
+int left = 0;
+int right = inputString.Length - 1;
+
+Console.WriteLine(CheckPalindromeRecursion(inputString, left, right));
+
+bool CheckPalindromeRecursion(string input, int left, int right)
+{
+    if (left >= right) //If pointers meet/cross, it is a palindrome. Stops recursion once half of the word is checked
+    {
+        return true;
+    }
+
+    if (input[left] != input[right]) //If characters don't match at any point, it's not a palindrome. Stops recursion immediately with false
+    {
+        return false;
+    }
+    
+    return CheckPalindromeRecursion(input, left + 1, right - 1); //If none of the prior conditions are met, checks to see if next iterations has an answer
+}
+
+#endregion
---
+++ Week 5 - Intro Data Structure and Algorithm/W5ChallengeLabs/ExtraLab2A/Program.cs
@@ -0,0 +1,44 @@
+﻿//Implement fibbonacci algorithm recursively and iteratively
+
+//Through Recursion
+Console.WriteLine("Fibonacci series using recursion:");
+
+int iterations = 8;
+
+for (int i = 0; i < iterations; i++)
+{
+    Console.Write(FibbonacciRecursive(i) + " "); //Calls on method starting at 0 and going up 
+}
+Console.WriteLine();
+
+int FibbonacciRecursive(int n)
+{
+    if (n <=1) //First and Second calls fall here, simply returning 0 and 1
+    {
+        return n;
+    }
+
+    return FibbonacciRecursive(n - 1) + FibbonacciRecursive(n - 2); //rest of the call fall here, returning the sum of the previous 2 numbers
+}
+
+Console.WriteLine("\n=======================================\n");
+
+//Through Iterations
+Console.WriteLine("Fibonacci series using iterative approach:");
+
+int val1 = 0;
+int val2 = 1; //0 and 1 are seeds, this loop cannot start at 0 or 1;
+int val3;
+int n;
+
+n = 8; //number of iterations
+
+Console.Write(val1 + " " + val2 + " "); //Starts iteration manually
+
+for (int i = 2; i < n; i++) //Call on for loop starting at i == 2
+{
+    val3 = val1 + val2;
+    Console.Write(val3 + " ");
+    val1 = val2; //Sliding window methodology, sliding val variables forward one slot so val3 can be recalculated
+    val2 = val3;
+}
