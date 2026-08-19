ce5d6a50a1d4bc079ab9c09e0b676a96ec0ab8c5
Matheus Castro <mpcastro0525@gmail.com>
2026-08-18 20:03:55 -0400

    - Added assignments 7.2.1 and 7.2.2

---
+++ Week 7 - DSA Sorting Expanded/Assignments_7.2/Assignment7.2.1/Program.cs
@@ -0,0 +1,90 @@
+﻿//Implement merge sort on an unsorted array of numbers. Take the array input from user.
+
+//Different methods to separate arrays and then merge them
+
+//Separate:
+//Identify middle element, separate into subarrays left and right
+//recursive until subarrays have one element
+
+//Merge:
+//Shuffling strategy - for each recursion, iterate through subawways merging pointers in lock-step
+//result will go into a new merged array, which will be passed up for the next step
+
+Console.WriteLine("Input elements (whole numbers) separated by a coma and space \", \"");
+string arrayInput = Console.ReadLine();
+int[] OriginalArr = arrayInput
+    .Split(", ") //Split input into substrings at each ", "
+    .Select(int.Parse) //Go through substrings trying to parse int elements to int
+    .ToArray(); //Conveys the result into an array
+
+Console.WriteLine($"Original array: {string.Join(',', OriginalArr)}");
+MergeSortDivide(OriginalArr, 0, OriginalArr.Length-1);
+Console.WriteLine($"after Merge sort: {string.Join(',', OriginalArr)}");
+
+void MergeSortDivide(int[] array, int left, int right) //Left and Right pointers to define the limits of the subArrays
+{
+    if (left >= right) //pointers crossed
+    {
+        return;
+    }
+
+    int middle = left + (right - left) / 2;
+
+    MergeSortDivide(array, left, middle); //Recursive action towards left of array
+    MergeSortDivide(array, middle+1, right); //Towards right of array
+
+    Merge(array, left, middle, right);
+}
+
+void Merge(int[] numbers, int left, int middle, int right)
+{
+    //determines the size of each new array and initializes them
+    int leftSize = middle - left + 1;
+    int rightSize = right - middle;
+
+    int[] leftArray = new int[leftSize];
+    int[] rightArray = new int[rightSize];
+
+    //populates the new arrays (not sorted yet)
+    for (int i = 0; i < leftSize; i++)
+        leftArray[i] = numbers[left + i];
+
+    for (int j = 0; j < rightSize; j++)
+        rightArray[j] = numbers[middle + 1 + j];
+
+    //Indexes (pointers) for the merge
+    int leftIndex = 0, rightIndex = 0; //equivalent to i, j
+    int mergedIndex = left; //left == beginning of array
+
+    //Sub arrays iteration preventing out of bounds
+    while (leftIndex < leftSize && rightIndex < rightSize)
+    {
+        if (leftArray[leftIndex] <= rightArray[rightIndex]) //actual sorting part
+        {
+            numbers[mergedIndex] = leftArray[leftIndex];
+            leftIndex++;
+        }
+        else
+        {
+            numbers[mergedIndex] = rightArray[rightIndex];
+            rightIndex++;
+        }
+
+        mergedIndex++; //advances main array pointer
+    }
+
+    //Takes care of remainders (if one array is bigger than the other)
+    while (leftIndex < leftSize)
+    {
+        numbers[mergedIndex] = leftArray[leftIndex];
+        leftIndex++;
+        mergedIndex++;
+    }
+
+    while (rightIndex < rightSize)
+    {
+        numbers[mergedIndex] = rightArray[rightIndex];
+        rightIndex++;
+        mergedIndex++;
+    }
+}
---
+++ Week 7 - DSA Sorting Expanded/Assignments_7.2/Assignment7.2.2/Program.cs
@@ -0,0 +1,124 @@
+﻿//Given a string s, reverse only all the vowels in the string and return it.
+
+//Vowels can appear in both lower and upper cases, and also appear more than once
+
+//Thoughts:
+//Create a string with vowels only and check if pointer is in that group (Contains?)
+//When first vowel is found, drop a pointer there and keep iterating
+//When second vowel is found, reverse the two
+
+string input = "intelligent";
+
+//ReverseVowelsBubble(input);
+
+Console.WriteLine(ReverseVowelsOrder(input));
+
+//Reverses the vowels in pairs
+void ReverseVowelsBubble(string input)
+{
+    Console.WriteLine($"Original input: {input}");
+
+    //creates a string with vowels in both cases for comparison
+    string vowels = "aeiouAEIOU";
+    char[] letters = input.ToCharArray();
+
+    int vowelPointer = 0;
+    bool firstVowel = true;
+
+    for (int i = 0; i < input.Length; i++)
+    {
+        if (vowels.Contains(input[i]) && firstVowel)
+        {
+            vowelPointer = i;
+            firstVowel = false;
+
+            continue;
+        }
+
+        if(vowels.Contains(input[i]))
+        {
+            char temp = letters[i];
+            letters[i] = letters[vowelPointer];
+            letters[vowelPointer] = temp;
+
+            firstVowel=true;
+            continue;
+        }
+    }
+
+    input = new(letters);
+    Console.WriteLine($"Inverted string: {input}");
+}
+
+//Reverse entire order of vowels
+string ReverseVowelsOrder(string input)
+{
+    Console.WriteLine(input);
+
+    //creates a string with vowels in both cases for comparison
+    string vowels = "aeiouAEIOU";
+    char[] letters = input.ToCharArray();
+
+    //create left and right pointers
+    int leftPointer = 0; 
+    int rightPointer = input.Length-1;
+
+    //Keeps track of where vowels happened, and if it happened on the counterside
+    int vowelPointer = 0;
+    bool vowelFound = false;
+
+    while (leftPointer < rightPointer)
+    {
+        if (vowels.Contains(input[leftPointer])) //found left vowel
+        {
+            if (vowelFound) //has right vowel waiting
+            {
+                //performs the swap and resets vowelFound to proceed
+                char temp = letters[rightPointer];
+                letters[rightPointer] = letters[vowelPointer];
+                letters[vowelPointer] = temp;
+
+                vowelFound = false; 
+
+                //Console.WriteLine($"Left vowel found at {leftPointer} SWAP");
+                leftPointer++;
+                continue;
+            }
+
+            vowelPointer = leftPointer; //If vowel is found but do not have a counterpart, drops pointer there and enables vowelFound
+            vowelFound=true;
+
+            //Console.WriteLine($"Left vowel found at {leftPointer}");
+
+        }
+
+        leftPointer++; //if letters[leftPointer] is not a vowel, simply move pointer forward
+
+        if (vowels.Contains(input[rightPointer])) //found right vowel
+        {
+            if(vowelFound) //has left vowel waiting
+            {
+                //performs the swap and resets vowelFound to proceed
+                char temp = letters[rightPointer];
+                letters[rightPointer] = letters[vowelPointer];
+                letters [vowelPointer] = temp;
+
+                vowelFound = false;
+
+                //Console.WriteLine($"Right vowel found at {rightPointer} SWAP");
+                rightPointer--;
+                continue;
+            }
+
+            vowelPointer = rightPointer; //If vowel is found but do not have a counterpart, drops pointer there and enables vowelFound
+            vowelFound = true;
+
+            //Console.WriteLine($"Right vowel found at {rightPointer}");
+        }
+
+        rightPointer--; //if letters[rightPointer] is not a vowel, simply move pointer backwards
+
+    }
+
+    return new string(letters);
+}
