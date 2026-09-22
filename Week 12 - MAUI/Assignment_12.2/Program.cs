2c2ec4a6da2f79c2c977f4495dd0b0099715386e
Matheus Castro <mpcastro0525@gmail.com>
2026-09-22 17:17:12 -0400

    Assignments of week 12 completed.

---
+++ Week 12 - MAUI/Assignment_12.2/Program.cs
@@ -0,0 +1,53 @@
+﻿//Given the head of a linked list and an integer val, remove all the nodes of the linked list that has Node.val == val, and return the new head.
+
+//Example 1:
+//Input: head = [1, 2, 6, 3, 4, 5, 6], val = 6
+//Output: [1, 2, 3, 4, 5]
+
+//Example 2:
+//Input: head = [], val = 1
+//Output: []
+
+//Example 3:
+//Input: head = [7, 7, 7, 7], val = 7
+//Output: []
+
+//Thoughts:
+//Before start iteration, check if head is null, if so return null.
+//Also before iteration, check if head.val == val, if so move head to head.next and recheck until head is not null and head.val != val.
+//Then start iteration, for each node, check if node.next.val == val, if so remove node.next without moving current node, else move current node to current.next.
+//Because we are checking node.next, we don't need to check the last node, so we can stop when node.next is null.
+
+LinkedList<int> SampleList = new LinkedList<int>(new int[] { 1, 2, 6, 3, 4, 5, 6 });
+int val = 6; //Expected output: [1, 2, 3, 4, 5]
+
+Console.WriteLine("Original List: " + string.Join(", ", SampleList));
+Console.WriteLine("Value to Remove: " + val);
+Console.WriteLine("Modified List: " + string.Join(", ", (RemoveValElements(SampleList, val) ?? Enumerable.Empty<int>())));
+
+LinkedList<int>? RemoveValElements(LinkedList<int> sampleList, int val)
+{
+    //Remove leading nodes with the target value
+    while (sampleList != null && sampleList.First.Value == val)
+    {
+        sampleList.RemoveFirst();
+    }
+
+    //While loop exits when first node that is not equal to val OR when head is null (all of the list nodes == val)
+    if (sampleList == null) return null;
+
+    var current = sampleList.First;
+    while (current != null && current.Next != null)
+    {
+        if (current.Next.Value == val)
+        {
+            current.List.Remove(current.Next);
+        }
+        else
+        {
+            current = current.Next;
+        }
+    }
+
+    return sampleList;
+}
---
+++ Week 12 - MAUI/Assignment_12.3/Program.cs
@@ -0,0 +1,51 @@
+﻿//(#1047) You are given a string s consisting of lowercase English letters.
+//A duplicate removal consists of choosing two adjacent and equal letters and removing them.
+//We repeatedly make duplicate removals on s until we no longer can.
+//Return the final string after all such duplicate removals have been made. It can be proven that the answer is unique.
+
+//Example 1:
+//Input: s = "abbaca"
+//Output: "ca"
+
+//Explanation:
+//In "abbaca" we could remove "bb" since the letters are adjacent and equal, and this is the only possible move.
+//The result of this move is that the string is "aaca", of which only "aa" is possible,
+//so the final string is "ca".
+
+//Example 2:
+//Input: s = "azxxzy"
+//Output: "ay"
+
+//Thoughts:
+//First instinct is a recursive solution, however, I can think of other ways as well.
+//I can use a pointer approach, keeping track of the current index and checking if the current character is equal to the next character.
+//If they are equal, I can remove both characters and move the pointer back one index to check for new adjacent duplicates.
+
+//Best way to do this is in a while loop, checking if the current index is less than (<) the length of the string - 1
+//This will naturally prevent an index out of range error when checking the next character.
+//Also, before performing i--, I need to check if i is greater than 0, otherwise I will get an index out of range error
+
+string s = "abbaca";
+List<char> stack = new(s);
+
+string RemoveAdjacentDuplicates(List<char> stack)
+{
+    int i = 0;
+    while (i < stack.Count - 1) //i < stack.Count - 1 to prevent index out of range (<= would always cause an error)
+    {
+        if (stack[i] == stack[i + 1])
+        {
+            //When RemoveAt is called, the list is re-indexed, so we need to remove the current index twice to remove both adjacent duplicates.
+            stack.RemoveAt(i);
+            stack.RemoveAt(i);
+
+            if (i > 0) i--; //If check to prevent index out of range error when i is 0.
+        }
+        else
+        {
+            i++;
+        }
+    }
+
+    return new string(stack.ToArray());
+}
